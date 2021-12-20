8bbcd55c42c5e9dfaac6ac214f5b5c8896c5c7a5
karimzakir02 <zakirkarim02@gmail.com>
2021-12-20 15:51:27 -0500

    Add enumeration class for store URLs

---
+++ webapp/url_enum.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class StoreURLs(StrEnum):
+    LOBLAWS = ("https://www.loblaws.ca/deals/all?sort=relevance&"
+               "category=27985&promotions=Price%20Reduction")
