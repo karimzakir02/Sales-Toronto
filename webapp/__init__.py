835df59b1e6606c496b406b7bedf3ec92a028aa9
karimzakir02 <zakirkarim02@gmail.com>
2021-12-19 21:32:51 -0500

    Add create_app method as seen in the Flask tutorial

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,26 @@
 from flask import Flask
+import os
 
-app = Flask(__name__)
 
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "webapp.sqlite"),
+    )
 
-@app.route("/")
-def home():
-    return "<p>Hello World! This is my upcoming data engineering project!</p>"
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def home():
+        return "<p>Hello! This is my upcoming data engineering project!</p>"
+
+    return app
