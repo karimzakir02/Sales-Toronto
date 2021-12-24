cf6f501b36c006760cad894abd599f6ec08a2551
karimzakir02 <zakirkarim02@gmail.com>
2021-12-24 11:42:45 -0500

    Add the retrieval of products data from the database

--- webapp/products.py
+++ webapp/products.py
@@ -1,10 +1,20 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, flash
 )
+from webapp.db import get_db
 
 bp = Blueprint("products", __name__, url_prefix="/products")
 
 
 @bp.route("/")
 def products_page():
-    return render_template("products/products.html")
+    db = get_db()
+    error = None
+
+    items = db.execute(
+        "SELECT * FROM products").fetchall()
+
+    if error is not None:
+        flash(error)
+
+    return render_template("products/products.html", items=items)
