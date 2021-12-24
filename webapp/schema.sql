09be11c0056830b1e69cfabc4998e9b62f248c6e
karimzakir02 <zakirkarim02@gmail.com>
2021-12-24 13:34:54 -0500

    Update schema to include date on which data was found

--- webapp/schema.sql
+++ webapp/schema.sql
@@ -7,5 +7,8 @@ CREATE TABLE products (
   current_price REAL NOT NULL,
   old_price REAL NOT NULL,
   in_stock INTEGER,
-  package_size TEXT
+  package_size TEXT,
+  year INT,
+  month INT,
+  day INT
 );
