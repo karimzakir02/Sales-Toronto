1f590325e7948e8a31d1f4c47c8bf923f0233c1e
karimzakir02 <zakirkarim02@gmail.com>
2021-12-26 11:42:42 -0500

    Add Metro website URL

--- webapp/url_enum.py
+++ webapp/url_enum.py
@@ -4,3 +4,4 @@ from strenum import StrEnum
 class StoreURLs(StrEnum):
     LOBLAWS = ("https://www.loblaws.ca/deals/all?sort=relevance&"
                "category=27985&promotions=Price%20Reduction")
+    METRO = ("https://www.metro.ca/en/flyer")
